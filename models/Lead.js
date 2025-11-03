4b1357b222eaea607ed0ec65df1072bcfec2dc9c
veerababu <you@example.com>
2025-11-03 17:32:10 +0530

    Made Course_interest optional in LeadModel False

--- models/Lead.js
+++ models/Lead.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const leadSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  course_interest: { type: String, required: fasle },
+  course_interest: { type: String, required: false },
   claimed_by: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', default: null }
 });
 module.exports = mongoose.model('Lead', leadSchema);
